043bd96b964187322b2ed5045036917ca8cfa8e3
idrissioxs <nuralievidiris777@gmail.com>
2026-02-01 23:01:22 +0600

    first Array

---
+++ src/main/java/org/example/Project.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+class Project {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter student name: ");
+        String name = sc.nextLine();
+
+        ArrayList<Integer> grades = new ArrayList<>();
+
+        System.out.print("Kl: ");
+        grades.add(sc.nextInt());
+
+        System.out.print("Ec: ");
+        grades.add(sc.nextInt());
+
+        System.out.print("Math: ");
+        grades.add(sc.nextInt());
+
+        System.out.print("Calculus: ");
+        grades.add(sc.nextInt());
+
+        int sum = 0;
+        for (int i = 0; i < grades.size(); i++) {
+            sum += grades.get(i);
+        }
+
+        double avg = (double) sum / grades.size();
+
+        System.out.println("Student: " + name);
+        System.out.println("Average: " + avg);
+    }
+}
